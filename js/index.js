0249af5803e93e4f3f4e0750ef204db6842191e6
Anton Khomenko <regiysa@gmail.com>
2023-04-01 12:14:41 +0300

    pre-deploy commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
 window.addEventListener('load', () => {
-
-    const [countUser, countComp, userField, compField, result, sound, play, unmuteBtn, muteBtn] = [
-        '.count-user', '.count-comp', '.user-field', '.comp-field',
-        '.result', '.sound', '.play','.sound-on', '.sound-off',
-    ].map((selector) => document.querySelector(selector));
+    const countUser = document.querySelector('.count-user');
+    const countComp = document.querySelector('.count-comp');
+    const userField = document.querySelector('.user-field');
+    const compField = document.querySelector('.comp-field');
+    const result = document.querySelector('.result');
+    const sound = document.querySelector('.sound');
+    const play = document.querySelector('.play');
+    const unmuteBtn = document.querySelector('.sound-on');
+    const muteBtn = document.querySelector('.sound-off');
+    const rules = document.querySelector('.rules-img');
+    const rulesBtn = document.querySelector('.rules-btn');
+    const closeBtn = document.querySelector('.close-btn');
+    const gameInfo = document.querySelector('.game-info');
     const fields = document.querySelectorAll('.field');
-    let userStep, compStep, userCountNum = 0, compCountNum = 0, blocked = false;
+    let userStep, compStep, compChoiceTimeout, userCountNum = 0, compCountNum = 0, blocked = false, restartInProgress = false;
 
     countUser.innerText = '0';
     countComp.innerText = '0';
 
+
+    rulesBtn.addEventListener('click', () => {
+        rules.classList.remove('hidden')
+        rules.classList.add('block');
+        closeBtn.classList.remove('hidden')
+        closeBtn.classList.add('block');
+    })
+
+    closeBtn.addEventListener('click', () => {
+        rules.classList.add('hidden')
+        rules.classList.remove('block');
+        closeBtn.classList.add('hidden')
+        closeBtn.classList.remove('block');
+    })
 
     unmuteBtn.addEventListener('click', () => {
         sound.muted = true;
@@ -23,7 +45,7 @@ window.addEventListener('load', () => {
 
 
 
-    function updateSoundButtons() {
+    const updateSoundButtons = () => {
         if (!sound.muted) {
             muteBtn.classList.add('hidden');
             unmuteBtn.classList.remove('hidden');
@@ -31,41 +53,41 @@ window.addEventListener('load', () => {
             muteBtn.classList.remove('hidden');
             unmuteBtn.classList.add('hidden');
         }
-    }
-
+    };
     updateSoundButtons();
 
 
     const userChoice = (e) => {
-        if (blocked) return;
+        if (blocked || restartInProgress) return;
         let target = e.target;
+
         if (target.classList.contains('field')) {
             userStep = target.dataset.field;
             fields.forEach(item => item.classList.remove('active', 'lose'))
             target.classList.add('active');
+            gameInfo.classList.remove('block')
+            gameInfo.classList.add('hidden')
             compChoice();
         }
     };
 
     const compChoice = () => {
-        blocked = true;
+        if (blocked || restartInProgress) return;
         let random = Math.floor(Math.random() * 3);
         compField.classList.add('pick-animation');
-        let compFields = compField.querySelectorAll('.field')
+        const compFields = compField.querySelectorAll('.field')
 
-        setTimeout(() => {
+        compChoiceTimeout = setTimeout(() => {
             compField.classList.remove('pick-animation');
             compStep = compFields[random].dataset.field;
             compFields[random].classList.add('active');
             winner();
         }, 3000);
 
-
     };
 
     const winner = () => {
         blocked = false;
-
         let conditions = userStep + compStep;
 
         switch (conditions) {
@@ -85,7 +107,7 @@ window.addEventListener('load', () => {
                 sound.play();
                 userCountNum++;
                 countUser.innerText = userCountNum;
-                compField.querySelector('[data-field = ' + compStep + ']').classList.add('lose');
+                compField.querySelector(`[data-field = ${compStep}]`).classList.add('lose');
                 break;
 
             case 'sr':
@@ -96,21 +118,30 @@ window.addEventListener('load', () => {
                 sound.play();
                 compCountNum++;
                 countComp.innerText = compCountNum;
-                userField.querySelector('[data-field = ' + userStep + ']').classList.add('lose');
+                userField.querySelector(`[data-field = ${userStep}]`).classList.add('lose');
                 break;
         }
     };
 
-    const startGame = () => {
+    const restartGame = () => {
+        if (restartInProgress) return;
+        restartInProgress = true;
         userCountNum = compCountNum = 0;
         result.innerText = 'Make your choice';
+        gameInfo.classList.remove('hidden')
+        gameInfo.classList.add('block')
         countUser.innerText = '0';
         countComp.innerText = '0';
         fields.forEach(item => item.classList.remove('active', 'lose'));
-
+        clearTimeout(compChoiceTimeout);
+        compField.classList.remove('pick-animation');
+        restartInProgress = false;
     };
 
-    play.addEventListener('click', startGame);
+
+
+
+    play.addEventListener('click', restartGame);
     userField.addEventListener('click', userChoice)
 
 
